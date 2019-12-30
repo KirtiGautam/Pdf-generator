602300abbb605ae3d188eaebda91244c40197f61
Riya <r.riyasingh.s15@gmail.com>
2019-12-31 01:50:42 +0530

    Bug Fix

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -8,6 +8,9 @@
     <title>Registration Form</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
+    body{
+        background: url("https://www.ppt-backgrounds.net/uploads/3d-slides-ppt.jpg");
+    }
         .p {
             padding: 1%;
         }
